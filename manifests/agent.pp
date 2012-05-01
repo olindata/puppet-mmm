c113dd4600ee468e85191e1f450904700b6872af
Walter Heck <walter@tribily.com>
2012-05-01 23:35:22 +0800

    Make /etc/mysql-mmm more managed..

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,27 +1,27 @@
 class mmm::agent {
-  
+
   include mmm::common
 
-  package { "mysql-mmm-agent":
-    ensure => 'present'
+  package { 'mysql-mmm-agent':
+    ensure  => 'present'
   }
-  
-  file { "/etc/mysql-mmm":
-    ensure => "directory",
-      mode    => 0755,
-      owner   => "root", 
-      group   => "root",
-    
-  }
-  file { "/etc/default/mysql-mmm-agent":
-    ensure  => present,
-    mode  => 0644,
-    owner  => "root",
-    group  => "root",
-    content   => template("mmm/agent-default.erb"),
-    require   => Package["mysql-mmm-agent"],
-  }  
 
-  
-  include mariadb::server      
+  file { '/etc/mysql-mmm':
+    ensure => 'directory',
+    mode   => 0755,
+    owner  => 'root',
+    group  => 'root',
+    purge  => true,
+  }
+  file { '/etc/default/mysql-mmm-agent':
+    ensure  => present,
+    mode    => 0644,
+    owner   => 'root',
+    group   => 'root',
+    content => template('mmm/agent-default.erb'),
+    require => Package['mysql-mmm-agent'],
+  }
+
+
+  include mariadb::server
 }
