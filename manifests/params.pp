c29fc54c247a8b7ab218eb01765338ee05f92232
Walter Heck <walterheck@olindata.com>
2013-10-17 21:13:13 +0300

    Add a bunch of params to make mmm multi-platform compatible

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,11 @@
 class mmm::params(
-  $multi_cluster_monitor = false
+  $pid_path,
+  $bin_path,
+  $status_path,
+  $multi_cluster_monitor = false,
+  $monitor_package = 'mysql-mmm-monitor',
+  $agent_package = 'mysql-mmm-agent',
+  $common_package = 'mysql-mmm-common',
 ) {
   
 }
